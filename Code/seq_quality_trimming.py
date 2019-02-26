cf4d952c853cd9adb5a018086425af9a7a93fe06
Sam K <samkim1729@gmail.com>
2019-02-26 15:01:50 -0500

    Implement merging

--- Code/seq_quality_trimming.py
+++ Code/seq_quality_trimming.py
@@ -36,6 +36,14 @@ FILENAMES = ["13E_001_BT-12S-fwd_BP005_F05",
              "15C_015_Rmicrosporus1050_NS7_G07",
              "15C_016_Rmicrosporus1050_ITS4_H07"]
 
+DNA_BASES = {"A":"T", "T":"A", "C":"G", "G":"C", "N":"N"}
+# Find the reverse complement of a DNA sequence.
+def rev_cmp(seq):
+    revcmp_seq = ""
+    for c in reversed(seq):
+        revcmp_seq += DNA_BASES[c]
+    return revcmp_seq
+
 # All paired forward/reverse sequences.
 # Each dict key is paired with a 2-tuple, corresponding to fwd/rev.
 # str: [{names, reads, seqs, quals, trimmed_seqs}]
@@ -44,26 +52,27 @@ for _i in range(0, len(FILENAMES), 2):
     _names = (FILENAMES[_i], FILENAMES[_i+1])
     _reads = (SeqIO.read(WD + _names[0] + ".ab1", "abi"),
               SeqIO.read(WD + _names[1] + ".ab1", "abi"))
-    _seqs = (str(_reads[0].seq), str(_reads[1].seq))
+    _seqs = (str(_reads[0].seq), rev_cmp(str(_reads[1].seq)))
     _quals = ([x for x in _reads[0].letter_annotations["phred_quality"]],
-              [x for x in _reads[1].letter_annotations["phred_quality"]])
+              [x for x in _reads[1].letter_annotations["phred_quality"]][::-1])
     sequences.append({"names" : _names,
                       "reads" : _reads,
                       "seqs"  : _seqs,
                       "quals" : _quals})
 
 
+    
 """ trim by quality scores """
 
 # Trim sequence by quality scores, using Kadane's algorithm.
-# Cutoff is lowest acceptable quality score, used to normalize the scores.
-# Gap penalty is penalty per nt of including a low-quality base, or "gap."
+# Cutoff is lowest acceptable quality score, used to normalize the scores around 0.
+# Gap penalty is penalty per nt of including a low-quality base.
 def trim_seq(seq, quality_scores, name=None, cutoff=None, gap_penalty=None):
     if cutoff is None:
         cutoff = np.average(quality_scores) * 0.9    # mean log, better cutoff metric needed
     if gap_penalty is None:
-        gap_penalty = cutoff * 3 # arbitrary gap penalty, seems to work
-    scores = [(x-cutoff if x > cutoff else x-cutoff-gap_penalty) for x in quality_scores]
+        gap_penalty = cutoff * -3 # arbitrary gap penalty, seems to work
+    scores = [(x-cutoff if x > cutoff else x-cutoff+gap_penalty) for x in quality_scores]
     
     # Kadane's algorithm
     max_to_here = 0
@@ -93,22 +102,21 @@ def trim_seq(seq, quality_scores, name=None, cutoff=None, gap_penalty=None):
     print("Avg score: %.2f (std %.2f, rng %d-%d, >=%d, -%d)\n"
           % (np.average(trimmed_scores), np.std(trimmed_scores),
              min(trimmed_scores), max(trimmed_scores), cutoff, gap_penalty))
-    return trimmed_seq, cutoff, gap_penalty
+    return trimmed_seq
 
 print("================== TRIMMING SEQUENCES ==================")
 
 for _sq in sequences:
-    _trim_fwd, _cut_fwd, _pen_fwd = trim_seq(_sq["seqs"][0], _sq["quals"][0], _sq["names"][0])
-    _trim_rev, _cut_rev, _pen_rev = trim_seq(_sq["seqs"][1], _sq["quals"][1], _sq["names"][1])
-    _sq["trimmed_seqs"] = (_trim_fwd, _trim_rev)
-    _sq["cutoffs"]      = (_cut_fwd, _cut_rev)
-    _sq["penalties"]    = (_pen_fwd, _pen_rev)
+    _sq["trimmed_seqs"] = (trim_seq(_sq["seqs"][0], _sq["quals"][0], _sq["names"][0]),
+                           trim_seq(_sq["seqs"][1], _sq["quals"][1], _sq["names"][1]))
+
 
 
 """ merge forward/reverse sequences """
 
-# Merge forward and reverse reads, using the Smith-Waterman algorithm.
-def merge_seqs(fwd, rev, name_fwd=None, name_rev=None, match=0, mismatch=0, gap_penalty=0):
+# Merge forward and reverse reads, using the Smith-Waterman algorithm.\
+# Different scores are assigned for matches, mismatches, and gaps in the alignment.
+def merge_seqs(fwd, rev, name_fwd=None, name_rev=None, match=None, mismatch=None, gap_penalty=None):
     # Build dictionary of scores for aligning bases
     dna_bases = ["A","T","C","G"]
     score_dict = {}
@@ -125,12 +133,12 @@ def merge_seqs(fwd, rev, name_fwd=None, name_rev=None, match=0, mismatch=0, gap_
     
     # Build tables
     max_align_score = 0
-    max_align_index = (0, 0)
+    max_back_index = (0, 0)
     for i in range(1, I):
         for j in range(1, J):
-            scores = (-gap_penalty + dp_table[i][j-1],  # left
+            scores = (gap_penalty + dp_table[i][j-1],  # left
                       score_dict[fwd[i-1]+rev[j-1]] + dp_table[i-1][j-1],   # diagonal
-                      -gap_penalty + dp_table[i-1][j],  # up
+                      gap_penalty + dp_table[i-1][j],  # up
                       0)    # quit
             dp_table[i][j] = max_score = max(scores)
             if max_score != 0:
@@ -139,15 +147,17 @@ def merge_seqs(fwd, rev, name_fwd=None, name_rev=None, match=0, mismatch=0, gap_
                         tb_table[i][j].append(k)
                 if max_score > max_align_score:
                     max_align_score = max_score
-                    max_align_index = (i, j)
+                    max_back_index = (i, j)
     
     # Merge forward, aligned middle, and reverse
-    merged_seq = fwd[:max_align_index[0]] + rev[max_align_index[1]:]
+    merged_seq = fwd[:max_back_index[0]] + rev[max_back_index[1]:]
     
     # Traceback
-    max_i, max_j = max_align_index
+    max_i, max_j = max_back_index
+    max_front_index = (0, 0)
     fwd_align = rev_align = ""
     while len(tb_table[max_i][max_j]) > 0:
+        max_front_index = (max_i, max_j)
         ptr = max(tb_table[max_i][max_j])
         if ptr != UP:
             if ptr == LEFT:
@@ -159,16 +169,24 @@ def merge_seqs(fwd, rev, name_fwd=None, name_rev=None, match=0, mismatch=0, gap_
                 rev_align = "-" + rev_align
             fwd_align = fwd[max_i-1] + fwd_align
             max_i -= 1
+    matches = 0
+    for i in range(len(fwd_align)):
+        if(fwd_align[i]==rev_align[i]):
+            matches += 1
     
     if name_fwd is not None and name_rev is not None:
         print(name_fwd + " + " + name_rev)
-    print("Trimmed seq: %d nt (%d-%d, %d%%)"
-          % (len(merged_seq), max_ind[0]+1, max_ind[1]+1, 100*len(trimmed_seq)/len(seq)))
-    print("Avg score: %.2f (std %.2f, rng %d-%d, >=%d, -%d)\n"
-          % (np.average(trimmed_scores), np.std(trimmed_scores),
-             min(trimmed_scores), max(trimmed_scores), cutoff, gap_penalty))
-    
-    print("\nUpmost optimal alignment:\n%s\n%s"   %(traceback(fwd, rev, tb_table, max_align_index)))
+    print("Merged seq: %d nt (overlap %d-%d trim_fwd, %d-%d trim_rev)"
+          % (len(merged_seq), max_front_index[0]+1, max_back_index[0]+1,
+             max_front_index[1]+1, max_back_index[1]+1))
+    print("Overlap: %d nt (%.2f%% match)\n"
+          % (max_back_index[0]-max_front_index[0]+1,
+             100*matches/len(fwd_align)))
+    return merged_seq
 
 print("================== MERGING SEQUENCES ===================")
 
+for _sq in sequences:
+    _sq["merged_seq"] = merge_seqs(_sq["trimmed_seqs"][0], _sq["trimmed_seqs"][1],
+                                   _sq["names"][0], _sq["names"][1],
+                                   2, -7, -7)   # arbitrary scores, seems to work
